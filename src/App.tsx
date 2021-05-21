5a8300525a3a2924241313da0fa32e737102148c
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-21 09:28:05 -0300

    building first custom component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 
+import ExpenseItem from "./components/ExpenseItem";
+
 const App: React.FC = () => {
   return (
     <div>
       <h2>Hello</h2>
+      <ExpenseItem />
     </div>
   );
 };
---
+++ src/components/ExpenseItem.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExpenseItem: React.FC = () => {
+  return <h2>Expense item</h2>;
+};
+
+export default ExpenseItem;
