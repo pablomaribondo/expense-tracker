b8c08a6c4dd3e6654b539bf9012b317b0a948b5e
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-21 10:11:44 -0300

    working with state

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 
 import { Expense } from "../../models/ExpenseItem.model";
@@ -6,13 +6,20 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 const ExpenseItem: React.FC<Expense> = ({ date, title, amount }) => {
+  const [expenseTitle, setExpenseTitle] = useState<string>(title);
+
+  const onClickHandler = () => {
+    setExpenseTitle("uiu");
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{expenseTitle}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
+      <button onClick={onClickHandler}>Change Title</button>
     </Card>
   );
 };
