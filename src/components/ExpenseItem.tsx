e9da28486f9f1dde6d7d41ef12763fdaaab69e7a
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-21 09:40:09 -0300

    passing data via props

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import "./ExpenseItem.css";
 
-const ExpenseItem: React.FC = () => {
-  const expenseDate = new Date(2021, 5, 21);
-  const expenseTitle = "Car Insurance";
-  const expenseAmount = 525.21;
+type ExpenseItemProps = {
+  date: Date;
+  title: string;
+  amount: number;
+};
 
+const ExpenseItem: React.FC<ExpenseItemProps> = ({ date, title, amount }) => {
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>{date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">${expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
       </div>
     </div>
   );
