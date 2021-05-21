55eea33833d667bc31e66c9e618a478b3ab7fbe4
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-21 09:46:49 -0300

    move expense date to its own component

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./ExpenseItem.css";
 
+import ExpenseDate from "./ExpenseDate";
+
 type ExpenseItemProps = {
   date: Date;
   title: string;
@@ -8,17 +10,9 @@ type ExpenseItemProps = {
 };
 
 const ExpenseItem: React.FC<ExpenseItemProps> = ({ date, title, amount }) => {
-  const month = date.toLocaleString("en-US", { month: "long" });
-  const day = date.toLocaleString("en-US", { day: "2-digit" });
-  const year = date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
