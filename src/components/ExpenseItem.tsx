cc0a05fc24b84bbbe910d10d94ef89c945870d85
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-21 09:41:54 -0300

    formatting date

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -8,9 +8,17 @@ type ExpenseItemProps = {
 };
 
 const ExpenseItem: React.FC<ExpenseItemProps> = ({ date, title, amount }) => {
+  const month = date.toLocaleString("en-US", { month: "long" });
+  const day = date.toLocaleString("en-US", { day: "2-digit" });
+  const year = date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
