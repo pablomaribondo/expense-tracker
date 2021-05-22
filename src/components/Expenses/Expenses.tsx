042ea8f9aa3a2daf3004b1709b454a7a17e60c3b
Pablo Maribondo <pablomgvieira@gmail.com>
2021-05-22 10:58:40 -0300

    rendering list of data

--- src/components/Expenses/Expenses.tsx
+++ src/components/Expenses/Expenses.tsx
@@ -19,26 +19,14 @@ const Expenses: React.FC<{ items: Expense[] }> = ({ items }) => {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={items[0].title}
-        amount={items[0].amount}
-        date={items[0].date}
-      />
-      <ExpenseItem
-        title={items[1].title}
-        amount={items[1].amount}
-        date={items[1].date}
-      />
-      <ExpenseItem
-        title={items[2].title}
-        amount={items[2].amount}
-        date={items[2].date}
-      />
-      <ExpenseItem
-        title={items[3].title}
-        amount={items[3].amount}
-        date={items[3].date}
-      />
+      {items.map(expense => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
     </Card>
   );
 };
